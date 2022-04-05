c15b42139c4e4f079e59e636c3f5c589274f2015
penpen0317 <penpen0317@gmail.com>
2022-04-05 20:18:04 +0900

    test

--- src/main.js
+++ src/main.js
@@ -1,24 +1,22 @@
-import '@babel/polyfill'
-import 'mutationobserver-shim'
 import Vue from 'vue'
-import './plugins/bootstrap-vue'
 import App from './App.vue'
+import './registerServiceWorker'
 import router from './router'
 import store from './store'
-
-// Amplify読み込み
+// ---------------↓ここから追記↓---------------
 import Amplify, * as AmplifyModules from 'aws-amplify'
+// @ts-ignore
 import { AmplifyPlugin } from 'aws-amplify-vue'
-import awsconfig from './aws-exports'
-Amplify.configure(awsconfig)
+// @ts-ignore
+import aws_exports from './aws-exports'
+Amplify.configure(aws_exports)
 
 Vue.use(AmplifyPlugin, AmplifyModules)
-
-
+// ---------------↑ここまで追記↑---------------
 Vue.config.productionTip = false
 
 new Vue({
-    router,
-    store,
-    render: h => h(App)
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
